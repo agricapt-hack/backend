f3414402555485f0f0d5d5cd62fc81ab1171ad3a
DEBASMITROY2002 <ddruk2018@gmail.com>
2025-08-16 02:23:37 +0530

    cicd fix with ec2 #6

---
+++ deploy_on_ec2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+DOCKER_FILE="$1"
+IMAGE_NAME="$2"
+APP_PORT="$3"
+
+# Install Docker if missing
+if ! command -v docker &> /dev/null
+then
+  echo "🚀 Installing Docker..."
+  sudo apt-get update
+  sudo apt-get install -y docker.io
+  sudo systemctl enable docker
+  sudo systemctl start docker
+  sudo usermod -aG docker $USER
+  echo "Docker installed and user added to docker group"
+fi
+
+docker stop "$IMAGE_NAME" || true
+docker rm "$IMAGE_NAME" || true
+docker build -f "$DOCKER_FILE" -t "$IMAGE_NAME:latest" .
+docker run -d --name "$IMAGE_NAME" -p "$APP_PORT:$APP_PORT" "$IMAGE_NAME:latest"
