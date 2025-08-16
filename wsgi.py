2206b272fed4e7a49c4cbaba761e93be0c34baae
DEBASMITROY2002 <ddruk2018@gmail.com>
2025-08-17 00:59:23 +0530

    http revert :-(

--- wsgi.py
+++ wsgi.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     application.run(
         host='0.0.0.0',
         port=8080,
-        ssl_context=('https_cert.pem', 'https_key.pem')  # Add your certificate and key file paths here
+        # ssl_context=('https_cert.pem', 'https_key.pem')  # Add your certificate and key file paths here
     )
